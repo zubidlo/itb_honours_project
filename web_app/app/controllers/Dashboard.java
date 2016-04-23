2424ce2c78a4b9dc4cf0fd54e8537d8ae4f6b032
Martin Zuber <zubidlo@gmail.com>
2016-04-23 12:31:19 +0100

    dashboard refactored

--- web_app/app/controllers/Dashboard.java
+++ web_app/app/controllers/Dashboard.java
@@ -1,53 +1,37 @@
 package controllers;
 
-import static play.data.Form.form;
 
 import java.io.*;
-import java.nio.file.*;
-import java.net.*;
-
-import controllers.Application.Login;
-import controllers.Application.Register;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import models.User;
-import models.utils.AppException;
-import play.Logger;
 import play.Play;
-import play.Environment;
-import play.Application;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 import play.twirl.api.*;
-import views.html.index;
-import views.html.dashboard.create;
-import views.html.dashboard.lecture;
+import views.html.dashboard.*;
 
 public class Dashboard extends Controller {
 
 	@Security.Authenticated(Secured.class)
     public Result index() {
-        return ok(create.render(User.findByEmail(request().username())));
+		User user = User.findByEmail(request().username());
+        return ok(index.render(user));
     }
     
 	@Security.Authenticated(Secured.class)
-    public Result lecture(String file) throws Exception {
-    	String email = ctx().session().get("email");
-    	if (email == null) {
-    		session().clear();
-    		return ok(index.render(form(Register.class), form(Login.class)));
-    	}
+    public Result lecture(String file) {
     	
-    	User user = User.findByEmail(email);
+    	User user = User.findByEmail(request().username());
     	String path = "public/lectures/" + file;
-        InputStream in = Play.application().classloader().getResourceAsStream(path);
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        StringBuffer fileContents = new StringBuffer();
-        String line = br.readLine();
-        while (line != null) {
-            fileContents.append(line);
-            line = br.readLine();
-        }
-        br.close();
-    	return ok(lecture.render(user, new Html(fileContents.toString())));
+    	try(InputStream in = Play.application().classloader().getResourceAsStream(path)) {
+    		Stream<String> lines = new BufferedReader(new InputStreamReader(in)).lines();
+            String html = lines.collect(Collectors.joining());
+            return ok(lecture.render(user, new Html(html.toString())));
+    	} catch (IOException e) { 
+    		e.printStackTrace();
+    		return notFound(index.render(user));
+    	}
     }
 }
